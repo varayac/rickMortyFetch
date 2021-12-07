b3a70bbb0491742863c725028a8254ba0bd752c8
varayac <victor.d.araya@gmail.com>
2021-12-07 16:44:03 -0300

    RickAndMorty V.0.1

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cards = document.getElementById("dynamic-cards");
 const templateCard = document.getElementById("template-card").content;
 const loading = document.getElementById("spinner-load");
 const api = "https://rickandmortyapi.com/api/character";
+const fragment = new DocumentFragment();
 
 //DOMContentLoaded makes sure the DOM loaded first.
 document.addEventListener("DOMContentLoaded", () => {
@@ -16,12 +17,26 @@ const fetchData = async () => {
     const res = await fetch(api);
     const data = await res.json();
 
-    console.log(data);
+    printCard(data);
   } catch (e) {
     console.log(e);
   } finally {
     loadingData(false);
   }
+};
+
+//Function paint Data Fetch results.
+const printCard = (data) => {
+  data.results.forEach((item) => {
+    console.log(item);
+    const clone = templateCard.cloneNode(true);
+    clone.querySelector("img").setAttribute("src", item.image);
+    clone.querySelector("h5").textContent = item.name;
+    clone.querySelector("p.text-secundary").textContent = item.species;
+    clone.querySelector("p.text-success").textContent = item.gender;
+    fragment.append(clone);
+  });
+  cards.append(fragment);
 };
 
 //Function painting spinner loading.
