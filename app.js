d1cdcb594e2731860b6941622483ad145c46f7b9
varayac <victor.d.araya@gmail.com>
2021-12-08 02:47:55 -0300

    RickAndMorty V.0.2

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 const cards = document.getElementById("dynamic-cards");
 const templateCard = document.getElementById("template-card").content;
 const loading = document.getElementById("spinner-load");
-const api = "https://rickandmortyapi.com/api/character";
+const pages = document.getElementById("nav-pages");
+const templatePages = document.getElementById("template-pages").content;
+//const api = "https://rickandmortyapi.com/api/character";
 const fragment = new DocumentFragment();
 
 //DOMContentLoaded makes sure the DOM loaded first.
@@ -14,10 +16,12 @@ const fetchData = async () => {
   try {
     loadingData(true);
 
-    const res = await fetch(api);
+    const res = await fetch(
+      (api = "https://rickandmortyapi.com/api/character")
+    );
     const data = await res.json();
 
-    printCard(data);
+    paintCard(data);
   } catch (e) {
     console.log(e);
   } finally {
@@ -25,10 +29,39 @@ const fetchData = async () => {
   }
 };
 
+//Function paint pages buttons
+const paintPages = (data) => {
+  console.log(data);
+  pages.textContent = "";
+  const clone = templatePages.cloneNode(true);
+
+  data.prev
+    ? (clone.querySelector(".btn-outline-secondary").disabled = false)
+    : (clone.querySelector(".btn-outline-secondary").disabled = true);
+
+  data.next
+    ? (clone.querySelector(".btn-outline-primary").disabled = false)
+    : (clone.querySelector(".btn-outline-primary").disabled = true);
+
+  pages.addEventListener("click", (e) => {
+    if (e.target.matches(".btn-outline-primary")) {
+      console.log("hola");
+      if (data.next) return fetchData(data.next);
+    }
+    if (e.target.matches(".btn-outline-secondary")) {
+      console.log("hola2");
+      if (data.prev) return fetchData(data.prev);
+    }
+  });
+
+  pages.append(clone);
+};
+
 //Function paint Data Fetch results.
-const printCard = (data) => {
+const paintCard = (data) => {
+  cards.textContent = "";
   data.results.forEach((item) => {
-    console.log(item);
+    //console.log(item);
     const clone = templateCard.cloneNode(true);
     clone.querySelector("img").setAttribute("src", item.image);
     clone.querySelector("h5").textContent = item.name;
@@ -37,6 +70,7 @@ const printCard = (data) => {
     fragment.append(clone);
   });
   cards.append(fragment);
+  paintPages(data.info);
 };
 
 //Function painting spinner loading.
